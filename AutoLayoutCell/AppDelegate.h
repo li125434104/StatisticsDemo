41c54d3d4e5b448ac624f14a82d222681d7d0ab9
lixiaojun <125434104@qq.com>
2019-11-05 17:12:31 +0800

    Initial Commit

---
+++ AutoLayoutCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AutoLayoutCell
+//
+//  Created by taishu on 2019/11/5.
+//  Copyright © 2019 taishu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AutoLayoutCell/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  AutoLayoutCell
+//
+//  Created by taishu on 2019/11/5.
+//  Copyright © 2019 taishu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
